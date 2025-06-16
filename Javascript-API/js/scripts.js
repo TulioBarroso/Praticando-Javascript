fcaa1f012c0bca67e0efd1a2f402f5f1fe4b5d60
Tulio Barroso <154523193+TulioBarroso@users.noreply.github.com>
2025-06-16 15:09:58 -0300

    Update ( 3 ) scripts.js - Simplificando o Código
    Simplificando o codigo, por só ter uma unica linha de retorno, é mais viavel não ter corpo { }

--- Javascript-API/js/scripts.js
+++ Javascript-API/js/scripts.js
@@ -7,13 +7,10 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 //Oque o fetch retorna? uma promise(um processamento assincrono, "não tenho" a resposta de imediato)
 fetch(url)  //Se o fetch dá certo, chama o .then
-.then((response)=>{ //Converter o body que está em readblestream para json
-     return response.JSON()
-})
-.then((jsonBody)=>{
-    console.log(jsonBody)
-}) 
-.catch((error)=>{ //caso dê algum erro
-      console.error(error) 
-})
+     
+.then((response)=> response.JSON()) //Converter o body que está em readblestream para json
+     
+.then((jsonBody)=> console.log(jsonBody))      
+     
+.catch((error)=> console.error(error)) //caso dê algum erro
 
