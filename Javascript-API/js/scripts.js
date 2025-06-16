8a37fb4469b368c5d8cc12d78ba5d4e151af367a
Tulio Barroso <154523193+TulioBarroso@users.noreply.github.com>
2025-06-16 15:20:32 -0300

    Update ( 5 ) scripts.js - convertendo para HTML
    Criando uma FUNCTION para converter os dados dos pokemons em html e um FOR para pecorrer a lista e convertendo cada um.

--- Javascript-API/js/scripts.js
+++ Javascript-API/js/scripts.js
@@ -5,14 +5,34 @@ const offset = 0 //ponto de partida
 const limit = 10 //quantidade de itens 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function converterPokemonHtml(pokemon){ //Função para converter os dados do pokemon, na estrutura html
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                </div>
+            </li>
+            `}
+
 //Oque o fetch retorna? uma promise(um processamento assincrono, "não tenho" a resposta de imediato)
 fetch(url)  //Se o fetch dá certo, chama o .then
      
 .then((response)=> response.JSON()) //Converter o body que está em readblestream para json
      
-.then((jsonBody)=> jsonBody.results))    
+.then((jsonBody)=> jsonBody.results)    
 
-.then((pokemonList)=> console.log(pokemonList))     
+.then((pokemons)=>{
+     
+     for(let i = 0; i<pokemons.length; i++){ //FOR para pecorrer a lista
+          const pokemon = pokemons[i];
+          pokemonList.innerHTML += converterPokemonHtml(pokemon)} 
+})     
 
 .catch((error)=> console.error(error)) //caso dê algum erro
 
