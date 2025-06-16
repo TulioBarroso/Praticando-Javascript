dee04d632af91f0af07e8f699aa83869838a9658
Tulio Barroso <154523193+TulioBarroso@users.noreply.github.com>
2025-06-16 14:57:17 -0300

    Update (1) scripts.js
    Requisição http inicial/basica com FETCH.

---
+++ Javascript-API/js/scripts.js
@@ -0,0 +1,18 @@
+//API com FETCH
+//POKE_API https://pokeapi.co/api/v2
+
+const offset = 0 //ponto de partida
+const limit = 10 //quantidade de itens 
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+//Oque o fetch retorna? uma promise(um processamento assincrono, "não tenho" a resposta de imediato)
+fetch(url)  //Se o fetch dá certo, chama o .then
+.then((response)=>{
+     console.log(response) 
+})
+.catch((error)=>{ //caso dê algum erro
+      console.error(error) 
+})
+.finally(()=>{  //Independente do sucesso ou fracasso, se eu quiser executar algo depois (finally)
+      console.log('Requisição concluida')
+}) 
