e52c4e3858adc79b66f99f99bab169f831fc8afd
Tulio Barroso <154523193+TulioBarroso@users.noreply.github.com>
2025-06-17 16:56:11 -0300

    Update ( 1 ) script.js - Criação da função do botão
    Criação da função do botão e otimização do codigo

--- Javascript-API-modelo-proprio/js/script.js
+++ Javascript-API-modelo-proprio/js/script.js
@@ -1,5 +1,14 @@
-function converterPokemonHtml(pokemon){
-    return `
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0
+
+
+
+const pokemonList = document.getElementById('pokemonList') //OL
+
+function loadPokemonItens(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = [])=>{
+    pokemonList.innerHTML += pokemons.map((pokemon)=> `
             <li class="pokemon ${pokemon.type}">
                 <span class="number">${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
@@ -12,17 +21,20 @@ function converterPokemonHtml(pokemon){
                     alt="${pokemon.name}">
                 </div>
             </li>
-            `}
-
-const pokemonList = document.getElementById('pokemonList') //OL
-
-pokeApi.getPokemons().then((pokemons = [])=>{
-    pokemonList.innerHTML += pokemons.map(converterPokemonHtml).join('')
+            `).join('')
  //Pega(innerHTML) a lista pokemon += mapeia essa lista, converte para html e junta esses li sem separador
 
  //IGUAL A -> 
  //const newHtml = pokemons.map(converterPokemonHtml).join(' ')
  //pokemonList.innerHTML = newHtml
+})
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', ()=> {
+    offset += limit
+    loadPokemonItens(offset, limit)  
 })
 
 
