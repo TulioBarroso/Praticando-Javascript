e881cce87a6fd2100038ad4ccd71e89fe8793a8c
Tulio Barroso <154523193+TulioBarroso@users.noreply.github.com>
2025-06-17 13:55:54 -0300

    Update ( 10 ) script.js
    convertendo os tipos dos pokemons numa lista e buscando imagens também.

--- Javascript-API organizado/js/script.js
+++ Javascript-API organizado/js/script.js
@@ -1,16 +1,21 @@
 //API COM FETCH
+//Convertendo os tipos do pokemon em uma lista
+function converterPokemonTypesHtml(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function converterPokemonHtml(pokemon){
     return `
             <li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
                 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${converterPokemonTypesHtml(pokemon.types).join('')}
                     </ol>
+                    <img src="${pokemon.sprites.other.dream_world.front_default}"
+                    alt="${pokemon.name}" width="100px">
                 </div>
             </li>
             `}
@@ -20,6 +25,10 @@ const pokemonList = document.getElementById('pokemonList') //OL
 pokeApi.getPokemons().then((pokemons = [])=>{
     pokemonList.innerHTML += pokemons.map(converterPokemonHtml).join('')
 //Pega a lista pokemon (innerHTML) += mapeia essa lista, converte para html e junta esses li sem separador
+
+//IGUAL A -> 
+ //const newHtml = pokemons.map(converterPokemonHtml).join(' ')
+ //pokemonList.innerHTML = newHtml
 })
 
 // const listItens = []
